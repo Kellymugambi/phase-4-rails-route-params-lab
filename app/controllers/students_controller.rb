178df60b8d6236d29e823215657f9a5d04852665
Kellymugambi <kellymgambi@.com>
2023-03-16 06:52:59 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,19 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+  
+    if params[:name].present?
+      name = params[:name].downcase
+      students = students.select { |student| student.first_name.downcase.include?(name) || student.last_name.downcase.include?(name) }
+    end
+    
     render json: students
+  end
+  
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
